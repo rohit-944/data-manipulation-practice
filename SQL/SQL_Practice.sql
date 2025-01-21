82d8ba0fe86d05ab63815516f3a31ac56c9a5593
rohit-944 <techrohit944@gmail.com>
2025-01-21 23:52:13 +0530

    SQL_Table

--- SQL/SQL_Practice.sql
+++ SQL/SQL_Practice.sql
@@ -1,1 +1,35 @@
 show databases;
+
+drop database practice
+
+create database College
+
+Show databases;
+
+use college
+
+
+CREATE TABLE Student (
+    id INT ,
+    name VARCHAR(100),
+    age INT,
+    address VARCHAR(255)
+);
+
+INSERT INTO Student (id, name, age, address) VALUES
+(1, 'John Doe', 20, '123 Main Street, New York, NY'),
+(2, 'Jane Smith', 22, '456 Elm Street, Los Angeles, CA'),
+(3, 'Michael Brown', 19, '789 Oak Avenue, Chicago, IL'),
+(4, 'Emily Davis', 21, '321 Pine Lane, Houston, TX'),
+(5, 'William Johnson', 23, '654 Maple Drive, Miami, FL');
+
+Show tables;
+
+Select * from Student;
+
+
+
+
+
+
+
